bbd3857a20ac5e1358cd5be66a04edeb2aa82281
Abderrahmane Sfaihi <abderrahmanesfaihi@hotmail.com>
2021-07-27 14:30:27 +0100

    NULL protection for ft_strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,29 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asfaihi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: asfaihi <asfaihi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/23 13:12:32 by asfaihi           #+#    #+#             */
-/*   Updated: 2019/11/06 15:58:50 by asfaihi          ###   ########.fr       */
+/*   Updated: 2021/07/27 14:29:03 by asfaihi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *s1)
+char	*ft_strdup(const char *s)
 {
 	int		i;
 	char	*a;
 
-	if (s1 == NULL)
+	if (s == NULL)
 		return (NULL);
-	a = malloc(ft_strlen(s1) + 1);
+	a = malloc(ft_strlen(s) + 1);
 	if (a == 0)
 		return (NULL);
 	i = 0;
-	while (s1[i])
+	while (s[i])
 	{
-		a[i] = s1[i];
+		a[i] = s[i];
 		i++;
 	}
 	a[i] = '\0';
